114b6d91ce1708fc146a00c728727f146252ded0
wxiwnd <wxiwnd@outlook.com>
2024-11-12 18:14:41 +0800

    change(gRPC): add gRPC calls for report_in and update_provider_status
    Signed-off-by: wxiwnd <wxiwnd@outlook.com>

--- grpc/supervisor/supervisor_pb2.py
+++ grpc/supervisor/supervisor_pb2.py
@@ -9,9 +9,13 @@ from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
-
 _runtime_version.ValidateProtobufRuntimeVersion(
-    _runtime_version.Domain.PUBLIC, 5, 27, 2, "", "supervisor.proto"
+    _runtime_version.Domain.PUBLIC,
+    5,
+    27,
+    2,
+    '',
+    'supervisor.proto'
 )
 # @@protoc_insertion_point(imports)
 
@@ -21,19 +25,19 @@ _sym_db = _symbol_database.Default()
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
-    b'\n\x10supervisor.proto\x12\x11kagami_supervisor\x1a\x1bgoogle/protobuf/empty.proto"C\n\x15WorkerReportInRequest\x12\x13\n\x0bworker_addr\x18\x01 \x01(\t\x12\x15\n\rworker_status\x18\x02 \x01(\x05"$\n\x10RegisterResponse\x12\x10\n\x08\x61\x63\x63\x65pted\x18\x01 \x01(\x08\x32\x62\n\nSupervisor\x12T\n\x10worker_report_in\x12(.kagami_supervisor.WorkerReportInRequest\x1a\x16.google.protobuf.Emptyb\x06proto3'
-)
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10supervisor.proto\x12\x11kagami_supervisor\x1a\x1bgoogle/protobuf/empty.proto\"C\n\x15WorkerReportInRequest\x12\x13\n\x0bworker_addr\x18\x01 \x01(\t\x12\x15\n\rworker_status\x18\x02 \x01(\x05\"$\n\x10RegisterResponse\x12\x10\n\x08\x61\x63\x63\x65pted\x18\x01 \x01(\x08\"M\n\x15UpdateProviderRequest\x12\x1b\n\x13provider_replica_id\x18\x01 \x01(\x05\x12\x17\n\x0fprovider_status\x18\x02 \x01(\x05\x32\xbe\x01\n\nSupervisor\x12T\n\x10worker_report_in\x12(.kagami_supervisor.WorkerReportInRequest\x1a\x16.google.protobuf.Empty\x12Z\n\x16update_provider_status\x12(.kagami_supervisor.UpdateProviderRequest\x1a\x16.google.protobuf.Emptyb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, "supervisor_pb2", _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'supervisor_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
-    DESCRIPTOR._loaded_options = None
-    _globals["_WORKERREPORTINREQUEST"]._serialized_start = 68
-    _globals["_WORKERREPORTINREQUEST"]._serialized_end = 135
-    _globals["_REGISTERRESPONSE"]._serialized_start = 137
-    _globals["_REGISTERRESPONSE"]._serialized_end = 173
-    _globals["_SUPERVISOR"]._serialized_start = 175
-    _globals["_SUPERVISOR"]._serialized_end = 273
+  DESCRIPTOR._loaded_options = None
+  _globals['_WORKERREPORTINREQUEST']._serialized_start=68
+  _globals['_WORKERREPORTINREQUEST']._serialized_end=135
+  _globals['_REGISTERRESPONSE']._serialized_start=137
+  _globals['_REGISTERRESPONSE']._serialized_end=173
+  _globals['_UPDATEPROVIDERREQUEST']._serialized_start=175
+  _globals['_UPDATEPROVIDERREQUEST']._serialized_end=252
+  _globals['_SUPERVISOR']._serialized_start=255
+  _globals['_SUPERVISOR']._serialized_end=445
 # @@protoc_insertion_point(module_scope)
