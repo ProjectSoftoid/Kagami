728ddc7ea887a64de1caca8bef1bd16bbc5b54f9
Ian Zhao <1758534277@qq.com>
2024-11-25 15:59:17 +0800

    feat(web-ui): init version
    Signed-off-by: Ian Zhao <1758534277@qq.com>

---
+++ kagami/webui/src/introduction_page/Firstsection/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link,useNavigate} from 'react-router-dom';
+import styles from './styles.module.scss';
+import LogoImage from '../../picture/logo.jpg';
+import WordImage from '../../picture/word.jpg';
+
+const Firstsection = () => {
+  const navigate = useNavigate();
+
+  const handleAdClick = () => {
+    navigate('/announcement');
+  };
+  const handleDownloadClick = () => {
+    navigate('/download');
+  }
+    const handleDomainClick = () => {
+        navigate('/domain');
+    }
+    const handleMirrorClick = () => {
+        navigate('/mirror');
+    }
+    const handleStateClick = () => {
+        navigate('/state');
+    }
+
+  return (
+    <div className={styles.Container}>
+      <div className={styles.firstSection}>
+        <div className={styles.logoSection}>
+          <img src={LogoImage} alt="logo" className={styles.logo} />
+          <img src={WordImage} alt="word" className={styles.word} />
+        </div>
+        <div className={styles.navLinks}>
+          <a onClick={handleMirrorClick} className={styles.mirror}>鏡像</a>
+          <a onClick={handleAdClick} className={styles.ad}>公告</a>
+          <a onClick={handleDownloadClick} className={styles.download}>下載</a>
+            <a onClick={handleDomainClick} className={styles.domain}>域名</a>
+            <a onClick={handleStateClick} className={styles.state}>狀態</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Firstsection;
---
+++ kagami/webui/src/page/Mirror.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link,useNavigate } from "react-router-dom";
+import Firstsection from '../introduction_page/Firstsection';
+
+
+
+
+
+
+const Mirrorpage=()=>{
+ 
+return(
+    <div className="Advertisementpage">
+      <Firstsection />
+      
+    </div>
+)
+}
+export default Mirrorpage;
