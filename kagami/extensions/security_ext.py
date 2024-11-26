f4a2cbff5b86cd0b89a68da279cd07e526952420
hedgehog098 <1445142129@qq.com>
2024-11-26 22:53:21 +0800

    change of salt

--- kagami/extensions/security_ext.py
+++ kagami/extensions/security_ext.py
@@ -8,3 +8,10 @@ class Encrypt:
             hashed_password=hashed_password.encode("utf-8"),
             password=raw_password.encode("utf-8"),
         )
+
+    @staticmethod
+    def salt_hashed_password(hashed_password: str, raw_password: str) -> str:
+        bytes = raw_password.encode("utf-8")
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(bytes, salt)
+        return hashed_password
