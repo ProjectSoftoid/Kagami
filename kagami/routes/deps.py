849e22b2732b9a0293dd7697a86a59ddee503de9
hedgehog098 <1445142129@qq.com>
2024-11-27 20:52:40 +0800

    feat(deps): add session lifespan manager deps
    Signed-off-by: hedgehog098 <1445142129@qq.com>

--- kagami/routes/deps.py
+++ kagami/routes/deps.py
@@ -1,8 +1,24 @@
+from contextlib import asynccontextmanager
 from typing import Annotated
 
 from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..core import Supervisor
+from ..database.engine import DatabaseEngine
 from ..server import supervisor
 
 SupervisorDeps = Annotated[Supervisor, Depends(lambda: supervisor)]
+
+
+@asynccontextmanager
+async def some_async_generator():
+    session: AsyncSession
+    session = DatabaseEngine.session_factory
+    try:
+        yield session
+    finally:
+        await session.close()
+
+
+SessionDeps = Annotated[AsyncSession, Depends(some_async_generator)]
