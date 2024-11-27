8ad374bf4c6dff7f59abf28030a081a3b0f5497f
wxiwnd <wxiwnd@outlook.com>
2024-11-27 19:56:53 +0800

    change: async support
    Signed-off-by: wxiwnd <wxiwnd@outlook.com>

--- kagami/database/engine.py
+++ kagami/database/engine.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-from ...config import SupervisorConfig
+from ..config import SupervisorConfig
 
 
 class DatabaseEngine:
