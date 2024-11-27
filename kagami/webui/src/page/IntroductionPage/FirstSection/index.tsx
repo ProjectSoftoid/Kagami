e76cc64677f158c777b22d1a681a4ff9859d02c2
SparrowHe <sparrowhe@gmail.com>
2024-11-27 13:23:11 +0800

    chore(web-ui): rename assets

--- kagami/webui/src/page/IntroductionPage/FirstSection/index.tsx
+++ kagami/webui/src/page/IntroductionPage/FirstSection/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link, useNavigate} from 'react-router-dom';
-import LogoImage from '../../../picture/logo.jpg';
-import WordImage from '../../../picture/word.jpg';
+import LogoImage from '../../../assets/logo.png';
+import WordImage from '../../../assets/word.png';
 import styles from './styles.module.scss';
 
 const Firstsection = () => {
