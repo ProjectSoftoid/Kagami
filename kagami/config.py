0e5a3b13153e22f1690442671659d99de865656b
wxiwnd <wxiwnd@outlook.com>
2024-11-29 01:09:47 +0800

    feat(core): entry_point of supervisor
    Signed-off-by: wxiwnd <wxiwnd@outlook.com>

--- kagami/config.py
+++ kagami/config.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings
 
 
 class SupervisorConfig(BaseSettings):
+    supervisor_host: str = "0.0.0.0"
+    supervisor_port: int = 21000
     grpc_host: str = "0.0.0.0"
     grpc_port: int = 21001
 
--- kagami/server.py
+++ kagami/server.py
@@ -1,3 +1,4 @@
+import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -5,8 +6,13 @@ from fastapi import FastAPI
 import grpc
 
 from ..grpc.supervisor import supervisor_pb2_grpc
+from .config import SupervisorConfig
 from .core import Supervisor
 from .routes import resource_router
+
+config = SupervisorConfig()
+logging.basicConfig(filename=config.log_file, level=config.log_level)
+logger = logging.getLogger(__name__)
 
 kagami_server = FastAPI()
 supervisor = None
@@ -16,6 +22,13 @@ supervisor = None
 async def lifespan(app: FastAPI):
     global supervisor
     supervisor = Supervisor  # TODO load the supervisor
+
+    logger.info("Reading config from config.env")
+    logger.info(f"gRPC Host: {config.grpc_host}")
+    logger.info(f"gRPC Port: {config.grpc_port}")
+    logger.info(f"Log File: {config.log_file}")
+    logger.info(f"Log Level: {config.log_level}")
+    logger.info(f"Database URL: {config.database_url}")
 
     # start gRPC server
     grpc_server = grpc.aio.server()
@@ -29,3 +42,10 @@ async def lifespan(app: FastAPI):
 
 kagami_server.router.lifespan = lifespan
 kagami_server.include_router(resource_router)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        app=kagami_server, host=config.supervisor_host, port=config.supervisor_port
+    )
