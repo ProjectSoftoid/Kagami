ae0f5712aad345b14a5e473613c214436d1e1051
wxiwnd <wxiwnd@outlook.com>
2024-12-04 20:50:35 +0800

    fix: fix config load
    Signed-off-by: wxiwnd <wxiwnd@outlook.com>

--- kagami/config.py
+++ kagami/config.py
@@ -1,16 +1,26 @@
-from pydantic_settings import BaseSettings
+from functools import lru_cache
+from pathlib import Path
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+@lru_cache
+def get_configs():
+    return SupervisorConfig()
 
 class SupervisorConfig(BaseSettings):
-    supervisor_host: str = "0.0.0.0"
-    supervisor_port: int = 21000
-    grpc_host: str = "0.0.0.0"
-    grpc_port: int = 21001
+    model_config = SettingsConfigDict(
+        env_prefix="kagami_supervisor_",
+        env_file=(Path("kagami", "config.env")),
+        env_ignore_empty=True,
+        extra="ignore",
+    )
 
-    log_file: str = "/var/log/kagami_worker.log"
+    http_host: str = "0.0.0.0"
+    http_port: int = 21000
+    grpc_host: str = "0.0.0.0"
+    grpc_port: int = 22000
+
+    log_file: str = "/var/log/kagami.log"
     log_level: str = "INFO"
     database_url: str = "change_me"
-
-    class Config:
-        env_prefix = "kagami_supervisor"
-        env_file = "config.env"
--- kagami/server.py
+++ kagami/server.py
@@ -5,25 +5,26 @@ from fastapi import FastAPI
 
 import grpc
 
-from .config import SupervisorConfig
+from .config import get_configs
 from .core import Supervisor
 from .grpc import supervisor_pb2_grpc
-from .routes import resource_router
 
-config = SupervisorConfig()
-logging.basicConfig(filename=config.log_file, level=config.log_level)
-logger = logging.getLogger(__name__)
-
-kagami_server = FastAPI()
+config = get_configs()
+logger = logging.getLogger("uvicorn.error")
 supervisor = None
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global supervisor
-    supervisor = Supervisor  # TODO load the supervisor
+    supervisor = Supervisor(
+        supervisor_host=config.grpc_host,
+        supervisor_port=config.grpc_port,
+        worker_info_list=[],
+    )  # TODO load the supervisor from database
 
-    logger.info("Reading config from config.env")
+    logger.info(f"HTTP Host: {config.http_host}")
+    logger.info(f"HTTP Port: {config.http_port}")
     logger.info(f"gRPC Host: {config.grpc_host}")
     logger.info(f"gRPC Port: {config.grpc_port}")
     logger.info(f"Log File: {config.log_file}")
@@ -36,8 +37,12 @@ async def lifespan(app: FastAPI):
     await grpc_server.start()
 
     yield
-    await grpc_server.stop()
+    await grpc_server.stop(0)
 
 
-kagami_server.router.lifespan = lifespan
+kagami_server = FastAPI(lifespan=lifespan)
+
+from .routes import resource_router  # noqa: E402
+
+# Include Router Area
 kagami_server.include_router(resource_router)
